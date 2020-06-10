08339f058f28c19e000b213792473afd64549dad
Paul Denning <padenning@outlook.com.au>
2020-06-10 21:26:59 +1000

    napalm 3.0.1 update

--- setup.py
+++ setup.py
@@ -1,20 +1,17 @@
 """setup.py file."""
-import uuid
-
 from setuptools import setup, find_packages
-try:
-    from pip._internal.req import parse_requirements
-except ImportError:
-    from pip.req import parse_requirements
 
 __author__ = 'Andreas Thienemann <andreas@bawue.net>'
 
-install_reqs = parse_requirements('requirements.txt', session=uuid.uuid1())
-reqs = [str(ir.req) for ir in install_reqs]
+with open("requirements.txt", "r") as fs:
+    reqs = [r for r in fs.read().splitlines() if (len(r) > 0 and not r.startswith("#"))]
+
+with open("README.md", "r") as fs:
+    long_description = fs.read()
 
 setup(
     name="napalm-procurve",
-    version="0.5.0",
+    version="0.6.0",
     packages=find_packages(),
     author="Andreas Thienemann",
     author_email="andreas@bawue.net",
